86bd3fa64c0ff544dfb0d74c186d593bc26a5f3d
Gena <genada89@gmail.com>
2026-04-07 21:14:56 +0300

    create cache deco

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    deco_cache = {}
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = tuple(args) + tuple(kwargs.items())
+        if key not in deco_cache:
+            deco_cache[key] = func(*args, **kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return deco_cache[key]
+    return wrapper
